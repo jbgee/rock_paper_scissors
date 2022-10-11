83550543eba571a85c92fe16197f78bda41040ac
Jacksongee <jacksonbgee@gmail.com>
2022-10-11 15:06:40 -0400

    Add first version of RPS UI. Simple buttons and Scoreboard, stops after first to 5.

---
+++ rps.js
@@ -0,0 +1,48 @@
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+rockButton.addEventListener('click',() => playRound('rock'));
+paperButton.addEventListener('click',() => playRound('paper'));
+scissorsButton.addEventListener('click',() => playRound('scissors'));
+
+let playerScore = 0;
+let computerScore = 0;
+function playRound(player){
+    if (playerScore <5 && computerScore <5){
+        const scoreboard = document.querySelector('#scoreboard');
+        const mostRecentResult = document.querySelector('#mostRecentResult')
+        computer = getComputerChoice()
+        if(computer == player){
+            mostRecentResult.textContent = "It's a tie, great minds think alike!";
+        }
+        else if((player == 'rock' && computer=='paper') || (player == 'scissors' && computer=='rock') || (player == 'paper' && computer=='scissors')){
+            mostRecentResult.textContent = `It looks like you lose! The computer chose ${computer}.`;
+            computerScore++;
+        }
+        else if((computer == 'rock' && player=='paper') || (computer == 'scissors' && player=='rock') || (computer == 'paper' && player=='scissors')){
+            mostRecentResult.textContent = `You Win! The computer chose ${computer} and you have come out victorious!`;
+            playerScore ++;
+        }
+        scoreboard.textContent = `Player:${playerScore} Computer:${computerScore}`
+        if (playerScore == 5)
+            mostRecentResult.textContent += "\n   You've won the first to 5! You've bested the computer!"
+        else if (computerScore ==5)
+            mostRecentResult.textContent += "\n   The computer won the first to 5! You are no match for the computer!"
+        return;
+    }
+
+}
+
+function getPlayerChoice(){
+    choice = prompt("Choose your weapon");
+    while(choice!= "rock" && choice != "scissors" && choice != "paper")
+        choice = prompt("Invalid choice, your weapon must be 'rock' 'paper', or 'scissors'")
+    return choice.toLowerCase();
+}
+
+function getComputerChoice(){
+    let choices = ['rock', 'paper', 'scissors']
+    let choice = choices[Math.floor(Math.random()*3)];
+    return choice;
+}
+
